c69345f20ed3f15fc416e295673e60b89bb6291b
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2024-04-22 11:32:13 +0500

    THIS MESSAGE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'node:path';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CardsComponent } from './cards/cards.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent 
+},
+{
+  path: "home",
+  component: HomeComponent 
+},
+{
+  path: "gallery",
+  component: GalleryComponent 
+},
+{
+  path: "cards",
+  component: CardsComponent 
+},
+{
+  path: "form",
+  component: FormComponent 
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
